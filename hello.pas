016571bee56b4288c32bd2a8a64002b8b5b7bd17
Irina Sviridova <irinasviridof@yandex.ru>
2023-10-08 14:07:00 +0300

    Initial commit

---
+++ hello.pas
@@ -0,0 +1,13 @@
+﻿begin
+  println('What language do you speak?');
+  var lang:=ReadString;
+  case lang of
+    'english': print('Hello!');
+    'russian': print('Здравствуйте!');
+    'german': print('guten Tag!');  
+  end;
+end.
+
+{What language do you speak? 
+english
+Hello! }
